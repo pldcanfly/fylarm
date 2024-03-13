1d79ac286e0a9999faa9ee3ef142d0e959affa61
Thomas Striegl <pldcanfly@gmail.com>
2024-03-13 13:55:57 +0100

    refactor: Moved Layout to components
    To keep the main.go lean and have the logic closer together. Mainly to
    not have to juggle return-type definitionsrefactor: Moved Layout to
    components

--- cmd/fylarm/main.go
+++ cmd/fylarm/main.go
@@ -1,35 +1,10 @@
 package main
 
 import (
-	"image/color"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/widget"
 	"github.com/pldcanfly/fylarm/internal/components"
 )
-
-func clocklayout(clock, date *canvas.Text) *fyne.Container {
-	return container.NewStack(
-		canvas.NewRectangle(color.Black),
-		container.NewGridWithRows(2,
-			container.NewVBox(
-				layout.NewSpacer(),
-				clock,
-				date,
-			),
-			container.NewVBox(
-				widget.NewLabelWithStyle("", fyne.TextAlignCenter, fyne.TextStyle{}),
-				widget.NewLabelWithStyle("Nächster Alarm: 12:34", fyne.TextAlignCenter, fyne.TextStyle{}),
-				widget.NewLabelWithStyle("Sonnig 24° Regenwahrscheinlichkeit: 50%", fyne.TextAlignCenter, fyne.TextStyle{}),
-				widget.NewLabelWithStyle("Heute in Graz", fyne.TextAlignCenter, fyne.TextStyle{}),
-			),
-		),
-	)
-}
 
 func main() {
 	a := app.New()
@@ -38,6 +13,6 @@ func main() {
 	w.Resize(fyne.NewSize(1920, 1080))
 	// w.SetFullScreen(true)
 	w.SetPadded(false)
-	w.SetContent(clocklayout(components.InitComponents()))
+	w.SetContent(components.Layout())
 	w.ShowAndRun()
 }
--- internal/components/components.go
+++ internal/components/components.go
@@ -6,9 +6,12 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
 )
 
-func InitComponents() (*canvas.Text, *canvas.Text) {
+func initComponents() (*canvas.Text, *canvas.Text) {
 	c := getClock()
 	d := getDate()
 
@@ -20,6 +23,43 @@ func InitComponents() (*canvas.Text, *canvas.Text) {
 	}()
 
 	return c, d
+}
+
+func Layout() *fyne.Container {
+	clock, date := initComponents()
+	return container.NewStack(
+		canvas.NewRectangle(color.Black),
+		container.NewGridWithRows(2,
+			container.NewVBox(
+				layout.NewSpacer(),
+				clock,
+				date,
+			),
+			container.NewVBox(
+				widget.NewLabelWithStyle(
+					"",
+					fyne.TextAlignCenter,
+					fyne.TextStyle{},
+				),
+
+				widget.NewLabelWithStyle(
+					"Nächster Alarm: 12:34",
+					fyne.TextAlignCenter,
+					fyne.TextStyle{},
+				),
+				widget.NewLabelWithStyle(
+					"Sonnig 24° Regenwahrscheinlichkeit: 50%",
+					fyne.TextAlignCenter,
+					fyne.TextStyle{},
+				),
+				widget.NewLabelWithStyle(
+					"Heute in Graz",
+					fyne.TextAlignCenter,
+					fyne.TextStyle{},
+				),
+			),
+		),
+	)
 }
 
 func getClock() *canvas.Text {
